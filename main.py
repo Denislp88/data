e33dcdb84d7a4bc05449e8c603fdf64e6e4dcf20
Denislp88 <dplekomtsev@gmail.com>
2022-10-19 16:26:07 +0400

    н

---
+++ main.py
@@ -0,0 +1,7 @@
+
+import mysql.connector
+
+cnx = mysql.connector.connect(user='Denis88', password='password89124662375Zaq1xsw2cde3',
+                              host='Denis88.mysql.pythonanywhere-services.com',
+                              database='Denis88$default')
+cnx.close()
