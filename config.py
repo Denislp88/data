c1d76d7976b5b97be4c20e061d6b0947f475cc89
Denislp88 <dplekomtsev@gmail.com>
2022-11-03 09:54:32 +0400

    3

--- config.py
+++ config.py
@@ -1,21 +1,4 @@
-host = "Denis88.mysql.pythonanywhere-services.com"
-user = "Denis88"
-password = "89124662375Zaq1xsw2cde3"
-db_name = "Denis88$default"
-
-
-
-
-   #
-from mysql.connector import connect, Error
-
-try:
-   with connect(
-            host="Denis88.mysql.pythonanywhere-services.com",
-            user="Denis88",
-            passwd="89124662375Zaq1xsw2cde3"
-    ) as connection:
-        print(connection)
-except Error as e:
-   print(e)
-   #
+host = "localhost"
+user = "root"
+password = "Zaq1xsw2cde3"
+db_name = "sakila"
